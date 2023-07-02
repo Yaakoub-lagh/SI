0e67fb7ffed91777d71e7e73d3f5498a89a8091b
yaakoub laghdache <laghdache.yaak@gmail.com>
2023-07-02 23:37:13 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import LoginForm from './login';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+      
+        <LoginForm />
+      </div>
+    );
+  }
+}
+
+export default App;
